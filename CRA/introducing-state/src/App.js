a26689039237c88e78b47169167801d05efbb3a9
Yachna Rana <yoyoyachu@Yachnas-MBP.home>
2021-06-16 23:49:19 -0400

    updating component with changing states

--- CRA/introducing-state/src/App.js
+++ CRA/introducing-state/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Game from './Game';
+import Randomnum from './Randomnum';
 
 function App() {
   return (
     <div className="App">
       <Game />
+      <Randomnum maxNum={10}/>
     </div>
   );
 }
--- CRA/introducing-state/src/Randomnum.js
+++ CRA/introducing-state/src/Randomnum.js
@@ -1,13 +1,19 @@
 import React,{Component} from 'react';
-class Game extends Component{
+class Randomnum extends Component{
     constructor(props){
         super(props);
         this.state = {
-            score : 10,
+            score : 0,
             gameOver: false
         }
+        this.makeTimer();
     }
-    
+    makeTimer(){
+        setInterval(()=>{
+            let randNum = this.state.score + 1;
+            this.setState({score:randNum})
+        },1000)
+    }
     render(){
         return(
             <div>
@@ -17,4 +23,4 @@ class Game extends Component{
     }
 }
 
-export default Game;
+export default Randomnum;
