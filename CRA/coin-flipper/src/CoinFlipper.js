fae346220c4f1fa38a78b609382d308cee186c9a
Yachna Rana <yoyoyachu@Yachnas-MBP.home>
2021-06-22 18:11:42 -0400

    stable version of coin flipper game

--- CRA/coin-flipper/src/CoinFlipper.js
+++ CRA/coin-flipper/src/CoinFlipper.js
@@ -1,52 +1,54 @@
 import React,{Component} from 'react';
-import Coin from './Coin'
-import './coinFlipper.css'
+import Coin from './Coin';
+import './coinFlipper.css';
 class CoinFlipper extends Component {
-    
     static defaultProps = {
-        // sidesArr:['https://tinyurl.com/react-coin-heads-jpg','https://tinyurl.com/react-coin-tails-jpg']
+        //array of images of coins
         sidesArr : ['https://raw.githubusercontent.com/JS-Beginners/coin-toss-game-2/master/heads.png','https://github.com/JS-Beginners/coin-toss-game-2/blob/master/tails.png?raw=true']
     }
+    //in state obj, player & computer refers to the side of coin assigning to them.
+    //playerScore & computerScore are counters whick keeps tracking of score whenever you flip the coin.
+    //announceMsg will display a msg of winning player
+    //by default, sideImg is set to a gif when no props is sent to Coin component(before flipping coin)
     constructor(props) {
         super(props);
         this.state = {  
             sideImg: 'https://media0.giphy.com/media/f9qwUQW56qC7BeCfCM/giphy.gif?cid=ecf05e47n93aitwrcqab3teqhjpbyunp2qm2jc8an08ixi4j&rid=giphy.gif&ct=g',
-            head: 0,
-            tail: 0,
             player: null,
             computer: null,
             playerScore: 0,
             computerScore: 0,
-            announceMsg: ''
+            announceMsg: '',
+            winMsg: ''
         }
     }
     
-    
+    //when player clicks on Heads btn, this fn assigns heads to player & tails to computer.
+    //also winMsg is set to empty string, just to make sure no msg is displayed after the game is over.
     playerHeads = () =>{
-        const idx = Math.floor(Math.random()*2);
-        this.setState({computer: idx})
-        this.setState({player: 0})
+        this.setState({player: 0,computer:1,winMsg:''})
     }
+
     playerTails = () =>{
-        const idx = Math.floor(Math.random()*2);
-        this.setState({computer: idx})
-        this.setState({player: 1})
+        this.setState({player: 1,computer:0,winMsg:''})
     }
+
     sides = ()=>{
+        //idx generates random num from 0 to 1 and oneSide will give element from this.props.sideArr...which provides img link
         const idx = Math.floor(Math.random()*2);
-        (idx === 0) ? this.setState({head: this.state.head + 1}) : this.setState({tail: this.state.tail + 1})
         const oneSide = this.props.sidesArr[idx];
+        //player & computer is set to null ,so no confusion happens before and after flipping coin
         this.setState({sideImg: oneSide,player:null,computer:null});
 
-        if(this.state.computer === idx && this.state.player === idx){
-            this.setState({announceMsg:"It's a tie"})
+        //takes the random num we've generated and compare it with player&computer's side(player,computer) to add score in particular one if matches
+        if(this.state.player === idx){
+            this.setState({playerScore: this.state.playerScore+1,announceMsg:'Point goes to player'})
         }else if(this.state.computer === idx){
             this.setState({computerScore: this.state.computerScore+1,announceMsg:'Point goes to computer'})
-        }else if(this.state.player === idx  ){
-            this.setState({playerScore: this.state.playerScore+1,announceMsg:'Point goes to player'})
         }
 
-        if(this.state.player >= 2){
+        //checks if anyone has score greater than 2 or not
+        if(this.state.playerScore >= 2){
             this.setState({winMsg:'You won the game',playerScore:0,computerScore:0})
         }
         if(this.state.computerScore >= 2){
@@ -60,6 +62,7 @@ class CoinFlipper extends Component {
         this.setState({sideImg: 'https://media0.giphy.com/media/f9qwUQW56qC7BeCfCM/giphy.gif?cid=ecf05e47n93aitwrcqab3teqhjpbyunp2qm2jc8an08ixi4j&rid=giphy.gif&ct=g',playerScore:0,computerScore:0,player:null,computer:null,announceMsg:'',winMsg:''})
     }
     render() { 
+        //playerTitle,computerTitle are defined temporarily to display which side assigns to which
         let playerTitle,computerTitle
         if(this.state.player === 0){
             playerTitle = 'Heads';
@@ -96,60 +99,19 @@ class CoinFlipper extends Component {
                                 <li>{computerTitle}</li>
                             </ul>                           
                         </li>
-                        
-                        
-
                     </ul>
-                    <div className="card-body">
-                    <button onClick={this.playerHeads} className="card-link">Heads</button>
-                    <button onClick={this.playerTails} className="card-link">Tails</button>
+                    <div className="card-body playerComputerLI">
+                    <button onClick={this.playerHeads} className="card-link btn  btn-primary btn-lg" data-bs-toggle="button">Heads</button>
+                    <button onClick={this.playerTails} className="card-link btn btn-success btn-lg" data-bs-toggle="button">Tails</button>
+                    <button onClick={this.playAgain} className="card-link btn btn-danger btn-lg">Play Again</button>
+
                     </div>
-                    <div className="card-body">
-                    <button onClick={this.sides} className="card-link">Flip Me!</button>
-                    <button onClick={this.playAgain} className="card-link">Play Again</button>
+                    
+                    <div className="card-body d-grid gap-4">
+                    <button onClick={this.sides} className="card-link btn btn-warning btn-lg">Flip Me!</button>
                     </div>
                 </div>
             </div>
-            
-            
-
-
-
-
-
-
-
-
-
-
-
-
-            // <div>
-            //     <h1>{this.state.winMsg}</h1>
-
-            //     <h1>{this.state.announceMsg}</h1>
-                
-            //     <div>
-            //         <h2>Player selected: {playerTitle}</h2>
-            //         <h2>Computer selected: {computerTitle}</h2>
-            //     </div>
-                
-            //     <Coin side={this.state.sideImg}/>
-            //     <div>
-            //     <button onClick={this.playerHeads} className="card-link">Heads</button>
-            //     <button onClick={this.playerTails} className="btn btn-warning">Tails</button>
-            //     </div>
-            //     <button onClick={this.sides} className="btn btn-success">Flip Me!</button>
-            //     <button onClick={this.playAgain} className="btn btn-danger">Play Again</button>
-
-
-            //     <div>
-            //         <h3>Player Score: {this.state.playerScore}</h3>
-            //         <h3>computer Score: {this.state.computerScore}</h3>
-
-            //     </div>
-                
-            // </div>
         );
     }
 }
