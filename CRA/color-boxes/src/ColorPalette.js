074a3e8dd905366d4653ba77ae12835881d3975e
yoyoyachu <ryachna@gmail.com>
2021-06-24 23:37:54 -0400

    passing number of boxes as props in ColorPalette component

--- CRA/color-boxes/src/ColorPalette.js
+++ CRA/color-boxes/src/ColorPalette.js
@@ -1,10 +1,15 @@
 import React, {Component } from 'react';
 import './colorPalette.css';
-import ColorBox from './ColorBox'
+// import ColorBox from './ColorBox';
+import Rboxes from './Rboxes';
+import Gboxes from './Gboxes';
+import Bboxes from './Bboxes';
 
 class ColorPalette extends Component {
     static defaultProps = {
-        numOfBoxes : 16
+        numOfBoxesR : 4,
+        numOfBoxesG : 4,
+        numOfBoxesB : 4
     }
     constructor(props) {
         super(props);
@@ -12,10 +17,15 @@ class ColorPalette extends Component {
         }
     }
     render() { 
-        return (  
-            <div className="color-palette-container">
-                {this.state.nums.map(n=><ColorBox />)}
-            </div>
+        const Redboxes = Array.from({length: this.props.numOfBoxesR}).map(m=><Rboxes />)
+        const Greenboxes = Array.from({length: this.props.numOfBoxesG}).map(m=><Gboxes />)
+        const Blueboxes = Array.from({length: this.props.numOfBoxesB}).map(m=><Bboxes />)
+        return (
+            <div className="container">
+                <div className="color-palette-container">
+                    {Redboxes}{Greenboxes}{Blueboxes}
+                </div>
+            </div>  
         );
     }
 }
